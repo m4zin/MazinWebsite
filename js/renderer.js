e8960ae39e6e69428e7238ca755f41f48ef1f1ae
hayzam <hayzam@gmail.com>
2022-03-06 14:36:36 +0530

    Render XML files

---
+++ js/renderer.js
@@ -0,0 +1,28 @@
+function loadXMLDocument(aboluteLocation) {
+    try {
+        xhttp = new XMLHttpRequest();
+        xhttp.open("GET", aboluteLocation, false);
+        xhttp.send();
+        return xhttp.responseXML;   
+    } catch (error) {
+        console.log(error)
+        alert("Could not lot XML file")
+    }
+}
+
+function renderXML() {
+    const xml = loadXMLDocument("./xml/index.xml");
+    const xsl = loadXMLDocument("./xml/index.xsl");
+
+    
+  let xsltProcessor = new XSLTProcessor();
+  xsltProcessor.importStylesheet(xsl);
+  
+  let resultDocument = xsltProcessor.transformToFragment(xml, document);
+  console.log(resultDocument)
+  document.getElementById("xml-view").appendChild(resultDocument);
+}
+
+document.addEventListener("DOMContentLoaded", async function(event) {
+    renderXML();
+})
